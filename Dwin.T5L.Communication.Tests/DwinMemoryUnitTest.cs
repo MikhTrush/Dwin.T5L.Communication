b5d1c29142271d4bda22795d59146881495053a9
uselessmash <uselessmash@gmail.com>
2025-08-08 14:31:12 +0700

    Changed project structure. No code changes

---
+++ Dwin.T5L.Communication.Tests/DwinMemoryUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dwin.T5L.Communication;
+using Xunit;
+
+namespace Dwin.T5L.Communication.Tests
+{
+    public class DwinMemoryUnitTest
+    {
+        DwinMemory dm;
+
+        public DwinMemoryUnitTest()
+        {
+            dm = new(new object());
+        }
+
+        [Theory]
+        [InlineData("string", "string")]
+        [InlineData("Руссиан текст", "Руссиан текст")]
+        public void WritesAndReadsString(string incoming, string result)
+        {
+            dm.WriteStringToMemory(incoming, 0x5000, Encoding.BigEndianUnicode);
+            var res = dm.ReadStringFromMemory(0x5000, (UInt16)(incoming.Length * 2), Encoding.BigEndianUnicode);
+            Assert.Equal(res, result);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
---
+++ Dwin.T5L.Communication.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Dwin.T5L.Communication.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
