188dd90094c21aae0f9cd85ff42964497557ba82
MikhTrush <uselessmash@gmail.com>
2025-09-11 16:38:59 +0700

    Updated constants

--- Dwin.T5L.Communication/DwinDisplayConstants.cs
+++ Dwin.T5L.Communication/DwinDisplayConstants.cs
@@ -1,7 +1,14 @@
 ﻿namespace Dwin.T5L.Communication
 {
+    /// <summary>
+    /// See the T5L_DGUS_II Application Development Guide
+    /// </summary>
     public class DwinDisplayConstants
     {
-        public static ushort PageNumberAddress { get; internal set; } = 0x84;
+        public const ushort SystemResetAddress = 0x04; //Length 2
+        public const ushort OSUpdateCmdAddress = 0x02; //Length 2
+        public const ushort NorFlashRwCmd = 0x08; //Length 4  
+        public const ushort UART2ConfigAddress = 0x0c; //Length 2
+        public const ushort PageNumberAddress = 0x84; //Length 2
     }
 }
