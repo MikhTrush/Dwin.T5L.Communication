821c2d78d61a77742bd74567f4e371716cf49b11
MikhTrush <uselessmash@gmail.com>
2025-09-12 09:01:11 +0700

    Updated LiveTests

--- Dwin.T5L.Communication.LiveTests/Program.cs
+++ Dwin.T5L.Communication.LiveTests/Program.cs
@@ -10,21 +10,13 @@ internal class Program
     {
         var s = SerialPort.GetPortNames()[0];
 
-        var sp = new SerialPort("/dev/ttyS2", 115200);
+        var sp = new SerialPort("COM18", 115200);
 
         Console.WriteLine("opened prt" + sp.BaudRate);
 
         dwin = new DwinDisplayNoMem(sp, 50, 2);
 
-        dwin.ClearMemory(0x5000, 0x0fff);
-
-
-
-
-        //foreach (var i in Enumerable.Range(0, 0xFF))
-        //{
-        //    dwin.GoToPage((ushort)i);
-        //}
+        dwin.ChangeDisplayBaudRate(806400);
 
         dwin.GoToPage(0x0);
         Thread.Sleep(1);
@@ -40,18 +32,24 @@ internal class Program
                 foreach (var v in values)
                 {
                     Console.Write($"{v} ");
-                    
+
                 }
 
                 Console.WriteLine();
-
-                Thread.Sleep(10);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
                 //throw;
             }
+            Thread.Sleep(1);
+
+            if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                break;
+            }
         }
+
+        dwin.ChangeDisplayBaudRate(115200);
     }
 }
